4d1098ffecc810c44fe213ace346f5463ecd6981
lestercomia <lestercomia@gmail.com>
2014-06-11 22:10:32 -0700

    halabira

---
+++ app/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		{{ HTML::style('_/css/bootstrap.css') }}
+		{{ HTML::style('_/css/mystyle.css') }}
+	</head>
+	<body>
+		<h2>hello</h2>
+		{{ HTML::script('_/js/bootstrap.js') }}
+		{{ HTML::script('_/js/myscript.js')}}
+	</body>
+</html>
