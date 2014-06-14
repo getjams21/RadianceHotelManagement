ec4d78d9a01710e0802dccd068f1868c8f292174
yeltsinpb <yeltsinpb@gmail.com>
2014-06-14 23:46:01 +0800

    gagu

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,8 +5,15 @@
 		{{ HTML::style('_/css/bootstrap.css') }}
 		{{ HTML::style('_/css/mystyle.css') }}
 	</head>
-	<body>
+	<body Background="_/fonts/images/bed.jpg"  bgproperties="fixed">
+			@yield('content')
+		@section('')
+		
+		@stop
 		{{ HTML::script('_/js/bootstrap.js') }}
 		{{ HTML::script('_/js/myscript.js')}}
 	</body>
 </html>
+<!doctype html>
+<html>
+
