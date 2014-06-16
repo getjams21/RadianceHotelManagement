1d43579a2bfd848a8db5c7206eb13c6bdbfb3091
lestercomia <lestercomia@gmail.com>
2014-06-16 21:54:24 +0800

    fixError

--- app/views/forms/rooms.blade.php
+++ app/views/forms/rooms.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.master')
-<<<<<<< HEAD
 @section('rooms')
 	<div class="col-xs-12 col-sm-6 col-md-8">
 		<div class="well formx">
@@ -17,26 +16,6 @@
 					</div>
 	  			</div>
 	  		</div>
-=======
-@section('form')
-		<div class="col-xs-12 col-sm-6 col-md-8">
-			<div class="well formx">
-				<div class="container-fluid">
-		  			<div class="row">
-	  					<center>
-	  						<div class="breadcrumb form-title-bc">
-	    						<font size="5">Rooms</font>
-	    					</div>
-	  					</center>
-		  			</div>
-		  			<div class="row">
-  						<div class="well">
-  							<h4>Rooms</h4>
-  						</div>
-		  			</div>
-		  		</div>
-			</div>
->>>>>>> 291ed755e5d0097fb4cd41939aa27b111feaa243
 		</div>
 	</div>
 @stop
