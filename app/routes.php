925eb014ccbf0b23197b41bab96d65d5065af92a
Jay Marz <algoisticdevteam@gmail.com>
2014-06-20 00:45:33 +0800

    Added boilerplate for search

--- app/routes.php
+++ app/routes.php
@@ -14,6 +14,7 @@
 Route::resource('page', 'PageController');
 Route::resource('rooms', 'RoomsController');
 Route::resource('settings', 'SidebarController');
+//Route::post('vacant', 'RoomsController@store');
 Route::get('vacant', 'RoomsController@vacant');
 Route::get('roomlist', 'RoomsController@roomlist');
 Route::get('/',function()
